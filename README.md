29b9258b4c50d46f2be9c9ed37599701cee05081
Donatas Laurinavicius <laurinavicius.donatas@gmail.com>
2021-07-02 16:45:39 +0300

    review

--- README.md
+++ README.md
@@ -1,12 +1,21 @@
-# k8s-task-docker-setup
-create 2 Docker images (front-end, backend) , that can be used later in kubernetes deployment
+# The task
+* In this task you will need to spin up front-end & backend apps in kubernetes. Both apps are stateless, you don't need to worry about persistent storage.
+* To get k8s (kubernetes) cluster you can use https://github.com/kubernetes/minikube; https://github.com/ubuntu/microk8s; or any other way to get your hands on k8s cluster.
+* Front-end & back-end apps cannot run in the same pod.
+* You will have to share your files that were needed to spin these apps on the k8s cluster (helm charts or ingress/deployment/... yaml files) and a readme how to use your files. Having your shared files and readme we should be able to spin these apps on our cluster.
 
 ## Docker images
+* front-end -> `igeolise/k8s-task-frontend:1.0.0`
+* back-end  -> `igeolise/k8s-task-backend:1.0.0`
 
-### Backend
+## Building docker images locally
+If you want to check source code of the images and build them yourself you can do this with:
+* run `DOCKER_BUILDKIT=1 docker build -t igeolise/k8s-task-backend:1.0.0 -f ./docker/back/Dockerfile .` to build back-end
+* run `DOCKER_BUILDKIT=1 docker build -t igeolise/k8s-task-frontend:1.0.0 -f ./docker/front/Dockerfile .` to build front-end
 
-Use following command to build docker image for backend  - `DOCKER_BUILDKIT=1 docker build -t igeolise/k8s-task-backend:1.0.0 -f ./docker/prod/back/Dockerfile .`
+## Diagram
+![alt text](diagram.png)
 
-### Frontend
-
-Use following command to build docker image for frontend  - `DOCKER_BUILDKIT=1 docker build -t igeolise/k8s-task-frontend:1.0.0 -f ./docker/prod/front/Dockerfile .`
+### Working app
+This is what you should see if you spin up apps successfully
+![alt text](app.png)
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,30 +1,29 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-
-app.use('*', (req, res, next) => {
-  console.log('Base URL');
+app.use("*", (req, res, next) => {
+  console.log("Base URL");
   console.log(req.baseUrl);
-  console.log('\nBody');
+  console.log("\nBody");
   console.log(req.body);
-  console.log('\n');
+  console.log("\n");
   next();
 });
 
-app.get('/api/is-alive', (req, res) => {
-  res.send(`Server is running. Last check - ${new Date().toLocaleString()}`)
+app.get("/api/is-alive", (req, res) => {
+  res.send(`Server is running. Last check - ${new Date().toLocaleString()}`);
 });
 
-app.post('/api/calculate', (req, res) => {
-  const { items }: { items: Array<{name: string, cost: number}>} = req.body;
+app.post("/api/calculate", (req, res) => {
+  const { items }: { items: Array<{ name: string; cost: number }> } = req.body;
   if (items) {
     const total = items.reduce(
-      (acc, curr) => ({ ...acc, cost: curr.cost + acc.cost}),
-      { name: 'total', cost: 0 },
+      (acc, curr) => ({ ...acc, cost: curr.cost + acc.cost }),
+      { name: "total", cost: 0 }
     );
 
     const randomDiscount = new Date().getSeconds();
@@ -32,11 +31,11 @@ app.post('/api/calculate', (req, res) => {
     res.send({
       total: total.cost.toFixed(2),
       discount: randomDiscount,
-      finalCost: (total.cost * (100 - randomDiscount) / 100).toFixed(2),
-    })
-  } else res.status(400).send('Badly formated request');
+      finalCost: ((total.cost * (100 - randomDiscount)) / 100).toFixed(2),
+    });
+  } else res.status(400).send("Badly formatted request");
 });
 
 app.listen(8080, () => {
-  console.log('Example app listening at http://localhost:8080');
+  console.log("App listening on port 8080");
 });
